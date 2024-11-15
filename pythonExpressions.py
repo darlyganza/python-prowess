a5dc9f49c4c09df865dd9f431722b72995367201
darlyganza <umumararunguganzadarlene@gmail.com>
2024-11-16 00:14:37 +0200

    Modified pythonExpressions.py (given examples of how expressions are carried out in python)

---
+++ pythonExpressions.py
@@ -0,0 +1,5 @@
+firstName=input("Enter first name: ")
+lastName=input("Enter last name: ")
+fullName=lastName+" "+firstName
+print(fullName+ "\nWhat can I do for you today?")
+# expressions in python help to combine values together be it values in variables or not:
