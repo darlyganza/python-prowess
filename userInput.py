898267c9c9d5a2e9ee5b6eb128b929e8267c0859
darlyganza <umumararunguganzadarlene@gmail.com>
2024-11-19 19:02:19 +0100

    updated userInput.py (showcased how the variables can aso used for receiving inputs)

--- userInput.py
+++ userInput.py
@@ -4,3 +4,6 @@ print(userName+ " How are you doing");
 
 # Asking the user to input some text use the input() from the standard libray
 input("Enter your name");
+# Saving the user's input in a variable: 
+name = input("Enter your name")
+print (name+ " How are you doing?")
