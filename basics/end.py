7d9c2be04865f7a6773e6ba629f4f89ccdc54847
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-11 15:43:58 +0100

    --allow-empty-message

--- basics/end.py
+++ basics/end.py
@@ -1,4 +1,4 @@
 # The end param of the input will be used to remove the default \n at the end of every print()
 
 print("The city was so beautiful", end=" and")
-print("The city was also lonely")
+print(" also lonely")
