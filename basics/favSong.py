6ecc1ca9171538b6d67ab8c48941583b121d8939
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-11 15:24:08 +0100

    --allow-empty-message

---
+++ basics/favSong.py
@@ -0,0 +1,5 @@
+userName = input("Hello what is your name? ")
+print ("Hi "+ userName+" It is nice to know you!🤗")
+print("What is your favorite song?")
+favSong = input();
+print("Woow "+userName+" I supprisingly love this song "+favSong+" too")
