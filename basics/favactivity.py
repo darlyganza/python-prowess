7e2fe4ae34afb7dbe714fa2596a057ab49ee9747
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-11 15:53:45 +0100

    --allow-empty-message

--- basics/favactivity.py
+++ basics/favactivity.py
@@ -1,1 +1,4 @@
-gi 
+useName = input("What is your name: ")
+favAct=input("Enter your favourite hobby: ")
+print("Hello", useName, " I din't know you liked",favAct,sep=' ,',end=' me ')
+print("too I like ",favAct)
