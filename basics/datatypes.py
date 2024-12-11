9af5af09894ab9d33c6c9a236dfdb8c5207e3991
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-11 16:11:42 +0100

    --allow-empty-message

---
+++ basics/datatypes.py
@@ -0,0 +1,6 @@
+int_a = 10
+float_a = 10.0
+string_a = '10'
+print("The integer is ",int_a," and the data type is ",type(int_a))
+print("The string is ",string_a," and the data type is ",type(string_a))
+print("The float is ",float_a," and the data type is ",type(float_a))
