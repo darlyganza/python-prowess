bdd1e01fcb8b78ad1e24b6125e3e70b3b3922a79
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-11 16:03:35 +0100

    --allow-empty-message

---
+++ basics/strings.py
@@ -0,0 +1,4 @@
+# Like many other programming languages: the '' and "" are used for storing python characters
+
+fullName = input("Enter any your full name: ")
+print("The full number of letters in your name is: ",len(fullName))
