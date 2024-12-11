b40e83d0e0e2daeb317eed8b98b38c799e09aa38
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-11 15:40:57 +0100

    --allow-empty-message

---
+++ basics/sep.py
@@ -0,0 +1,5 @@
+# The sep will deal with telling the characters to be between all the strings:
+
+# This will print every block of string followed by the ,
+print ("Hello"," World", sep=";")
+print("I love to drink ","milk","water","banana",sep=';')
