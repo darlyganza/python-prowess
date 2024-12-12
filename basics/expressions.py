33e071f44dee23bd0fa798df14577180e9d233a3
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-12 19:55:46 +0100

    showed the order of expressions and their strength

---
+++ basics/expressions.py
@@ -0,0 +1,8 @@
+# Python expressions follow the order of strength where some expressions like in mathematics BODMAS are strictly
+# followed
+print ((12-3)*3)
+# the brackets are of more importance in this case
+print(12**2)
+# Double * for expressions
+
+
