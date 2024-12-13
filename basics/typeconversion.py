b97516f9d7a002b2d158573ad36224cfa9641f0f
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-13 19:59:33 +0100

    described more about the implicit type conversion and the explicit type conversion

---
+++ basics/typeconversion.py
@@ -0,0 +1,9 @@
+# Implicit type conversion
+x = 12
+y=x+1.6
+print (y,type(y))
+# Python will automatically assign this to a float type
+
+# Explicit type conversion (I need to explicitly state the data type:
+userSalary = int(input("Enter your salary: "))
+print(userSalary,type(userSalary))
