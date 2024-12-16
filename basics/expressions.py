c206afbfaa62db0b533d87ba24736d69fdb1fa5d
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-16 21:07:01 +0200

    demonstrated the floor division

--- basics/expressions.py
+++ basics/expressions.py
@@ -5,6 +5,8 @@ print ((12-3)*3)
 print(12**2)
 # Double * for expressions
 
+# floor division (no points left after division
+print(13//2)
 # Rounding off floating point numbers:
 x = 123.4457923
 print(round(x,2))
