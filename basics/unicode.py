e374ea2e80c28fc8cfcdf57d83ec41c0abb0a906
darlyganza <umumararunguganzadarlene@gmail.com>
2024-12-18 09:24:33 +0200

    demonstrated the use of escape characters

--- basics/unicode.py
+++ basics/unicode.py
@@ -2,3 +2,5 @@ print(ord('P'))
 print(chr(44032))
 
 # The ASCII range for Korean language: 50000
+print("Young girls and \nYoung boys are polite")
+print("And thus Jesus said:\"You are lucky if they torture you because of my name\"")
